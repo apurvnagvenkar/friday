40f97b0026b7a018ee9f2327e8a125fdcced2d69
apurvnagvenkar <apurv.nagvenkar@gmail.com>
2016-12-04 18:00:35 +0530

    intent classification done

---
+++ lib/entities.py
@@ -0,0 +1,10 @@
+__author__ = 'haptik'
+
+def entity_detection(msg):
+    """
+    run the entity detection over a message
+    :param msg:
+    :return:
+    """
+
+    return {}
---
+++ lib/main.py
@@ -0,0 +1,45 @@
+__author__ = 'haptik'
+
+
+
+####
+# Run the entity detection
+# Run the intent identification
+
+
+# if bot is asking:
+    #  if entity is present for that question
+        # Reply
+        # answerd_flag =True
+    #   else:
+        # answerd_flag = False
+
+
+
+# If position of domain is > current position
+    # over_excited = True
+# else:
+    # call api_functionality
+    # api_call = True
+    #response
+    # increment position
+
+# if answerd_flag ==  False and api_call=True
+    # why you not answering my question?
+    # unanswered_question +=1
+    # set current domain to prevous intent
+
+
+# if intent is not changed
+    #bot ask a question
+    # send response
+    # ask_question +=1
+
+
+# if ask_question > threshold?
+    # you are boring me dont want to chat with you Why always i have to ask a question
+# if unanswered _question >  threshold?
+    # why you not answering my questions?
+
+
+
