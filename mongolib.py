d60c6edb50d880507ad510d4569fe17869b4b059
Ubuntu <ubuntu@ip-172-31-41-188.us-west-2.compute.internal>
2016-12-11 00:00:35 +0000

    changes made

--- mongolib.py
+++ mongolib.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import urllib2
 client = MongoClient()
 client = MongoClient('mongodb://localhost:27017/')
 db = client['june']
@@ -21,8 +22,8 @@ def get_user_conversation(user_id):
 
 def update_user_info(user_id, data):
     result = user_info.update(
-	{ user_id: user_id },
-	{"$set":data},
+	{ "user_id": user_id },
+        {"$set":data},
         True
     )
 
@@ -34,6 +35,31 @@ def update_user_conversation(user_id, data):
 	True
     )
 
-update_user_info("sachin",{"user_id":"sachin","name":"sach"})
-result = get_user_info("sachin")
-print result
+def delete_user_conversation(user_id):
+    result = user_conversation.delete_one(
+	{ "user_id": user_id }
+    )
+
+
+#def get_age():
+#    response = urllib2.urlopen('https://graph.facebook.com/v2.8/me?fields=birthday&access_token=EAAPogLiQapYBAKVEkcGfsZCHN25uHhJRjOZBJ8mJDi47MJbpVErJZBgBIK7nZAe43aIKDjGGryPhwvDb9CZB2vbLOPKCjGLlrjSDB0mGlsXRYYmvUwOPvt8j147zkZCcIik4D6ZCLwJKcX7XFt94jSmBSfoh4rX0ZBwn4iwn8NZA7GQZDZD'
+
+def start_over(user_id):
+    data = {
+        'user_id' : user_id,
+        user_id : {
+            'name': 'apurvaa',
+            'info': {
+                'age': None,
+                'age_verify': 25,
+                'occupation': None,
+                'city': None,
+                'studied': None,
+                'home_town': None,
+                'movie': None,
+            }
+        }
+    }
+    update_user_info(user_id,data)
+    delete_user_conversation(user_id)
+    print get_user_info(user_id)
